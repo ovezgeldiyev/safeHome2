bd2eb3f58573d1c731076b73dc0f910d48befe7a
ovezgeldiyev <ovezgeldiyevagabay2000@gmail.com>
2024-06-13 15:04:35 +0500

    asd

---
+++ js/main.js
@@ -0,0 +1,90 @@
+// menu start
+var menu = document.getElementById("menu");
+var menuBtn = document.getElementById("menuBtn");
+var body = document.body;
+menuBtn.onclick = function () {
+  menu.classList.toggle("active");
+  menuBtn.classList.toggle("active");
+  body.classList.toggle("active");
+};
+window.onclick = function (event) {
+  if (event.target == menu) {
+    menu.classList.remove("active");
+    menuBtn.classList.remove("active");
+    body.classList.remove("active");
+  }
+};
+// menu end
+
+// scroll start
+let header = document.getElementById("header");
+function scrollFunc() {
+  if (window.scrollY >= 200) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+window.onscroll = function () {
+  scrollFunc();
+};
+// scroll end
+// faq start
+const tabBtn = document.querySelectorAll(".tabBtn");
+const tabEvent = document.querySelectorAll(".tabEvent");
+tabBtn.forEach((e) => {
+  onTabClick(tabBtn, tabEvent, e);
+});
+function onTabClick(tabBtns, tabItems, item) {
+  item.addEventListener("click", function (e) {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute("data-tab");
+    let currentTab = document.querySelector(tabId);
+    if (currentBtn.classList.contains("active")) {
+      currentBtn.classList.remove("active");
+      currentTab.classList.remove("active");
+    } else if (!currentBtn.classList.contains("active")) {
+      tabBtns.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      tabItems.forEach(function (item) {
+        item.classList.remove("active");
+      });
+      currentBtn.classList.add("active");
+      currentTab.classList.add("active");
+    }
+  });
+}
+// faq end
+// faq start
+const faqBtn = document.querySelectorAll(".faqBtn");
+const faqEvent = document.querySelectorAll(".faqEvent");
+faqBtn.forEach((e) => {
+  onFaqClick(faqBtn, faqEvent, e);
+});
+function onFaqClick(faqBtns, faqItems, item) {
+  item.addEventListener("click", function (e) {
+    let currentBtn = item;
+    let faqId = currentBtn.getAttribute("data-faq");
+    let currentTab = document.querySelector(faqId);
+    if (currentBtn.classList.contains("active")) {
+      const faq = currentBtn.parentElement.querySelector(".faqEvent");
+      if (faq) {
+        faq.classList.remove("active");
+        currentBtn.classList.remove("active");
+      }
+    } else if (!currentBtn.classList.contains("active")) {
+      faqBtn.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      faqItems.forEach(function (item) {
+        item.classList.remove("active");
+      });
+      currentBtn.classList.add("active");
+      currentTab.classList.add("active");
+    }
+  });
+}
+// faq end
